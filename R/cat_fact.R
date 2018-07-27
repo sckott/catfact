74dad97070db8c8850e10f2c32a8e7b6c8a21d6f
Scott Chamberlain <myrmecocystus@gmail.com>
2018-07-26 18:57:41 -0700

    add more tests and fixtures

--- R/cat_fact.R
+++ R/cat_fact.R
@@ -11,6 +11,8 @@
 #' cat_fact('asdf') # invalid max_length returns nothing
 #' }
 cat_fact <- function(max_length = NULL, ...) {
+  assert(max_length, c('integer', 'numeric'))
+
   args <- cp(list(max_length = max_length))
   res <- httr::GET("https://catfact.ninja/fact", query = args, ...)
   httr::stop_for_status(res)
@@ -18,3 +20,12 @@ cat_fact <- function(max_length = NULL, ...) {
 }
 
 cp <- function(x) Filter(Negate(is.null), x)
+
+assert <- function(x, y) {
+  if (!is.null(x)) {
+    if (!class(x)[1L] %in% y) {
+      stop(deparse(substitute(x)), " must be of class ",
+           paste0(y, collapse = ", "), call. = FALSE)
+    }
+  }
+}
--- tests/testthat/test-cat_fact.R
+++ tests/testthat/test-cat_fact.R
@@ -10,3 +10,30 @@ test_that("cat_fact works", {
     expect_type(aa$length, 'integer')
   })
 })
+
+test_that("cat_fact length works", {
+  vcr::use_cassette("cat_fact_lengths", {
+      expect_lte(cat_fact(30)$length, 30)
+      expect_lte(cat_fact(80)$length, 80)
+      expect_lte(cat_fact(120)$length, 120)
+      expect_lte(cat_fact(300)$length, 300)
+    }, 
+    match_requests_on = c('method', 'uri', 'query'),
+    record = "new_episodes"
+  )
+})
+
+test_that("cat_fact error behavior", {
+  vcr::use_cassette("cat_fact_zero", {
+      aa <- cat_fact(max_length = 0)
+
+      expect_is(aa, "list")
+      expect_equal(length(aa), 0)
+    }, 
+    match_requests_on = c('method', 'uri', 'query'), 
+    preserve_exact_body_bytes = TRUE
+  )
+
+  expect_error(cat_fact(max_length = 'foobar'))
+})
+
